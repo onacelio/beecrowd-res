404897205d559f539920c1087057a910b75efefc
O Nacélio! <86494087+onacelio@users.noreply.github.com>
2023-01-30 13:51:40 -0300

    Update 1059.js

--- 1059.js
+++ 1059.js
@@ -2,6 +2,6 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 for (let i = 1; i <= 100; i++) {
-		if(i % 2 == 0)
-			console.log(i)
+	if(i % 2 == 0)
+		console.log(i)
 }
